92c8bb7d18d7e787fe3a8014e69a4b69a5bb6eed
Stéphane Senart <stephane.senart@gmail.com>
2019-08-23 14:54:48 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from distutils.core import setup
+import setuptools
+
 setup(
   name = 'pyveoliaidf',         # How you named your package folder (MyLib)
   packages = ['pyveoliaidf'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Retrieve water consumption from Veolia Ile-de-France web site (French Water Company)',   # Give a short description about your library
-  author = 'Stéphane Senart',                   # Type in your name
+  author = 'Stephane Senart',                   # Type in your name
   author_email = 'stephane.senart@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ssenart/PyGazpar',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/ssenart/PyGazpar/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['RESOURCE', 'WATER', 'CONSUMPTION'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'selenium',
-          'time',
-          'csv',
-          'os'
+          'selenium == 3.141'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    'Intended Audience :: Developers',      # Define that your audience are developers
@@ -25,3 +24,4 @@ setup(
     'Programming Language :: Python :: 3.7',    
   ],
 )
+
