776c534f39257cb0659bfee9b157d104199f73a5
Stéphane Senart <stephane.senart@gmail.com>
2019-08-25 23:25:33 +0200

    Close WebDriver even in case of exceptions

--- pyveoliaidf/client.py
+++ pyveoliaidf/client.py
@@ -38,53 +38,57 @@ class Client(object):
         profile.set_preference('browser.download.dir', self.__tmp_directory)
         profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
         
-        driver = webdriver.Firefox(executable_path=self.__firefox_webdriver_executable, firefox_profile=profile, options=options, service_log_path=self.__tmp_directory + '/geckodriver.log')
-        driver.set_window_position(0, 0)
-        driver.set_window_size(1200, 1200)
-        
-        driver.implicitly_wait(10)
-        
-        driver.get(HOME_URL)
-        
-        # Fill login form
-        email_element = driver.find_element_by_css_selector("input[type='email']")
-        password_element = driver.find_element_by_css_selector("input[type='password']")
-        
-        email_element.send_keys(self.__username)
-        password_element.send_keys(self.__password)
-        
-        submit_button_element = driver.find_element_by_class_name('submit-button')
-        submit_button_element.click()
-        
-        # Wait a few for the login to complete
-        time.sleep(5)
-        
-        # Page 'votre consomation'
-        driver.get(DATA_URL)
+        try:
+            driver = webdriver.Firefox(executable_path=self.__firefox_webdriver_executable, firefox_profile=profile, options=options, service_log_path=self.__tmp_directory + '/geckodriver.log')
+            driver.set_window_position(0, 0)
+            driver.set_window_size(1200, 1200)
+            
+            driver.implicitly_wait(10)
+            
+            driver.get(HOME_URL)
+            
+            # Fill login form
+            email_element = driver.find_element_by_css_selector("input[type='email']")
+            password_element = driver.find_element_by_css_selector("input[type='password']")
+            
+            email_element.send_keys(self.__username)
+            password_element.send_keys(self.__password)
+            
+            submit_button_element = driver.find_element_by_class_name('submit-button')
+            submit_button_element.click()
+            
+            # Wait a few for the login to complete
+            time.sleep(5)
+            
+            # Page 'votre consomation'
+            driver.get(DATA_URL)
 
-        # Wait a few for the data page load to complete
-        time.sleep(10)
-        
-        # Download file
-        download_button_element = driver.find_element_by_xpath("//button[contains(.,'Télécharger la période')]")
-        download_button_element.click()
-        
-        # Wait a few for the download to complete
-        time.sleep(10)
-        
-        # Close the driver
-        driver.close()
-        
-        # Load the CSV file into the data structure
-        with open(data_file_path, 'r') as csvfile:
-            dictreader = csv.DictReader(csvfile, delimiter=';', fieldnames=['time', 'total_liter', 'daily_liter', 'type'])
-            # Skip the header line
-            next(dictreader.reader)
-            for row in dictreader:
-                self.data.append(dict(row))
+            # Wait a few for the data page load to complete
+            time.sleep(10)
+            
+            # Download file
+            download_button_element = driver.find_element_by_xpath("//button[contains(.,'Télécharger la période')]")
+            download_button_element.click()
+            
+            # Wait a few for the download to complete
+            time.sleep(10)
 
-        # Close the file
-        csvfile.close()
+            # Load the CSV file into the data structure
+            with open(data_file_path, 'r') as csvfile:
+                dictreader = csv.DictReader(csvfile, delimiter=';', fieldnames=['time', 'total_liter', 'daily_liter', 'type'])
+                # Skip the header line
+                next(dictreader.reader)
+                for row in dictreader:
+                    self.data.append(dict(row))
 
-        # Remove the file
-        os.remove(data_file_path)
+            # Close the file
+            csvfile.close()
+
+            # Remove the file
+            os.remove(data_file_path)
+            
+        finally:
+            # Close the driver
+            driver.close()
+        
+
