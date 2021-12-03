78fe3506d02cb993160603da733bf6d40843bbb8
Stéphane Senart <stephane.senart@gmail.com>
2021-12-03 14:57:52 +0100

    [Pipeline] improvements

--- pyveoliaidf/__init__.py
+++ pyveoliaidf/__init__.py
@@ -1,3 +1,4 @@
-from pyveoliaidf.enum import PropertyNameEnum
-from pyveoliaidf.client import Client
-from pyveoliaidf.client import LoginError
+from pyveoliaidf.enum import PropertyNameEnum  # noqa: F401
+from pyveoliaidf.client import Client  # noqa: F401
+from pyveoliaidf.client import LoginError  # noqa: F401
+from pyveoliaidf.version import __version__  # noqa: F401
